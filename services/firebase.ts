f841121d7909bc8dbec93f3dd98cfac23089894e
nutritude <suanutritude@gmail.com>
2026-02-24 16:04:09 -0300

    feat: remove firebase config hardcoding to enforce real multi-device sync v2.0.11

--- services/firebase.ts
+++ services/firebase.ts
@@ -3,12 +3,12 @@ import { getFirestore } from "firebase/firestore";
 import { getAuth } from "firebase/auth";
 
 export const firebaseConfig = {
-    apiKey: import.meta.env.VITE_FIREBASE_API_KEY || "AIzaSyCaBGmBq4Hj6IJA0765q6kDKk6JWG4_Sws",
-    authDomain: import.meta.env.VITE_FIREBASE_AUTH_DOMAIN || "controclin-602b6.firebaseapp.com",
-    projectId: import.meta.env.VITE_FIREBASE_PROJECT_ID || "controclin-602b6",
-    storageBucket: import.meta.env.VITE_FIREBASE_STORAGE_BUCKET || "controclin-602b6.firebasestorage.app",
-    messagingSenderId: import.meta.env.VITE_FIREBASE_MESSAGING_SENDER_ID || "510250226894",
-    appId: import.meta.env.VITE_FIREBASE_APP_ID || "1:510250226894:web:265ccdd6f92c55ca656c9f"
+    apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
+    authDomain: import.meta.env.VITE_FIREBASE_AUTH_DOMAIN,
+    projectId: import.meta.env.VITE_FIREBASE_PROJECT_ID,
+    storageBucket: import.meta.env.VITE_FIREBASE_STORAGE_BUCKET,
+    messagingSenderId: import.meta.env.VITE_FIREBASE_MESSAGING_SENDER_ID,
+    appId: import.meta.env.VITE_FIREBASE_APP_ID
 };
 
 export let firebaseError = false;
